a5680c9e483dcf799bf4636a4f83af8415502028
Jasonix <ccjjxx99@qq.com>
2022-02-14 17:06:00 +0800

    重构数据集生成，防止train、valid、test数据集服务器变化导致结果错误

--- data/eua_dataset.py
+++ data/eua_dataset.py
@@ -5,13 +5,10 @@ from data.data_generator import DataGenerator
 
 class EuaTrainDataset(Dataset):
 
-    def __init__(self, user_num, data_num, x_start_prop, x_end_prop, y_start_prop, y_end_prop, device,
-                 rate=3, min_cov=1, max_cov=1.5):
-        self.generator = DataGenerator(user_num, rate)
-        servers = self.generator.init_server(x_start_prop, x_end_prop, y_start_prop, y_end_prop, min_cov, max_cov)
+    def __init__(self, servers, users_list, users_within_servers_list, users_masks_list, device):
         self.servers = torch.tensor(servers, dtype=torch.float32, device=device)
-        self.users_list, self.users_within_servers_list, self.users_masks_list =\
-            self.generator.init_users_list_by_server(servers, data_num, True, max_cov)
+        self.users_list, self.users_within_servers_list, self.users_masks_list = \
+            users_list, users_within_servers_list, users_masks_list
         self.device = device
 
     def __len__(self):
@@ -30,7 +27,7 @@ class EuaTestDataset(Dataset):
         self.generator = DataGenerator(user_num, rate)
         servers = self.generator.init_server(x_start_prop, x_end_prop, y_start_prop, y_end_prop, min_cov, max_cov)
         self.servers = torch.tensor(servers, device=device)
-        self.users_list, self.users_within_servers_list, self.users_masks_list =\
+        self.users_list, self.users_within_servers_list, self.users_masks_list = \
             self.generator.init_users_list_by_server(self.servers, data_num, False, max_cov)
         self.device = device
 
@@ -51,3 +48,35 @@ class EuaTestDataset(Dataset):
         mask_seq = torch.tensor(mask_seq, device=self.device)
 
         return self.servers, user_seq, mask_seq
+
+
+def generate_three_set(user_num, data_num, x_start_prop, x_end_prop, y_start_prop, y_end_prop, device,
+                       rate=3, min_cov=1, max_cov=1.5):
+    """
+    :param user_num:
+    :param data_num: 数组，包括训练、验证、测试三个数
+    :param x_start_prop:
+    :param x_end_prop:
+    :param y_start_prop:
+    :param y_end_prop:
+    :param device:
+    :param rate:
+    :param min_cov:
+    :param max_cov:
+    :return:
+    """
+    generator = DataGenerator(user_num, rate)
+    servers = generator.init_server(x_start_prop, x_end_prop, y_start_prop, y_end_prop, min_cov, max_cov)
+    users_list, users_within_servers_list, users_masks_list = \
+        generator.init_users_list_by_server(servers, data_num[0], load_sorted=True, max_cov=max_cov)
+    train_set = EuaTrainDataset(servers, users_list, users_within_servers_list, users_masks_list, device)
+
+    users_list, users_within_servers_list, users_masks_list = \
+        generator.init_users_list_by_server(servers, data_num[1], load_sorted=True, max_cov=max_cov)
+    valid_set = EuaTrainDataset(servers, users_list, users_within_servers_list, users_masks_list, device)
+
+    users_list, users_within_servers_list, users_masks_list = \
+        generator.init_users_list_by_server(servers, data_num[2], load_sorted=True, max_cov=max_cov)
+    test_set = EuaTrainDataset(servers, users_list, users_within_servers_list, users_masks_list, device)
+
+    return train_set, valid_set, test_set
--- run/train.py
+++ run/train.py
@@ -3,9 +3,9 @@ import time
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-from data.eua_dataset import EuaTrainDataset
 from nets.attention_net import PointerNet
 from util.utils import log_and_print
+from data.eua_dataset import generate_three_set
 
 if __name__ == '__main__':
     batch_size = 1024
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     dropout = 0.5
     server_reward_rate = 0.1
     user_num = 100
-    resource_rate = 2
+    resource_rate = 1.5
     x_end = 0.4
     y_end = 0.5
     train_size = 100000
@@ -27,39 +27,37 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
-    print("正在加载训练数据集")
-    filename = "../dataset/train_server_" + str(x_end) + "_" + str(y_end) + "_user_" \
-               + str(user_num) + "_rate_" + str(resource_rate) + "_size_" + str(train_size) + ".pkl"
+    train_filename = "D:/transformer_eua/dataset/train_server_" + str(x_end) + "_" + str(y_end) + "_user_" \
+                     + str(user_num) + "_rate_" + str(resource_rate) + "_size_" + str(train_size) + ".pkl"
+    valid_filename = "D:/transformer_eua/dataset/valid_server_" + str(x_end) + "_" + str(y_end) + "_user_" \
+                     + str(user_num) + "_rate_" + str(resource_rate) + "_size_" + str(valid_size) + ".pkl"
+    test_filename = "D:/transformer_eua/dataset/test_server_" + str(x_end) + "_" + str(y_end) + "_user_" \
+                    + str(user_num) + "_rate_" + str(resource_rate) + "_size_" + str(test_size) + ".pkl"
+
     try:
-        with open(filename, 'rb') as f:
-            train_set = pickle.load(f)  # read file and build object
-    except FileNotFoundError:
-        train_set = EuaTrainDataset(user_num, train_size, 0, x_end, 0, y_end, device, rate=resource_rate)
-        with open(filename, 'wb') as f:
+        print("正在加载训练数据集")
+        with open(train_filename, 'rb') as f:
+            train_set = pickle.load(f)
+        print("正在加载验证数据集")
+        with open(valid_filename, 'rb') as f:
+            valid_set = pickle.load(f)
+        print("正在加载测试数据集")
+        with open(test_filename, 'rb') as f:
+            test_set = pickle.load(f)
+    except FileNotFoundError as e:
+        print("文件{}未找到，重新生成".format(e.filename))
+        train_set, valid_set, test_set = \
+            generate_three_set(user_num, (train_size, valid_size, test_size),
+                               0, x_end, 0, y_end, device, rate=resource_rate)
+        with open(train_filename, 'wb') as f:
             pickle.dump(train_set, f)
-    print("加载训练数据集完成")
-
-    filename = "../dataset/valid_server_" + str(x_end) + "_" + str(y_end) + "_user_" \
-               + str(user_num) + "_rate_" + str(resource_rate) + "_size_" + str(valid_size) + ".pkl"
-    try:
-        with open(filename, 'rb') as f:
-            valid_set = pickle.load(f)  # read file and build object
-    except FileNotFoundError:
-        valid_set = EuaTrainDataset(user_num, valid_size, 0, x_end, 0, y_end, device, rate=resource_rate)
-        with open(filename, 'wb') as f:
+            print("保存训练集成功")
+        with open(valid_filename, 'wb') as f:
             pickle.dump(valid_set, f)
-    print("加载验证数据集完成")
-
-    filename = "../dataset/test_server_" + str(x_end) + "_" + str(y_end) + "_user_" \
-               + str(user_num) + "_rate_" + str(resource_rate) + "_size_" + str(test_size) + ".pkl"
-    try:
-        with open(filename, 'rb') as f:
-            test_set = pickle.load(f)  # read file and build object
-    except FileNotFoundError:
-        test_set = EuaTrainDataset(user_num, test_size, 0, x_end, 0, y_end, device, rate=resource_rate)
-        with open(filename, 'wb') as f:
+            print("保存验证集成功")
+        with open(test_filename, 'wb') as f:
             pickle.dump(test_set, f)
-    print("加载测试数据集完成")
+            print("保存测试集成功")
 
     train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
     valid_loader = DataLoader(dataset=valid_set, batch_size=batch_size, shuffle=False)
@@ -111,12 +109,15 @@ if __name__ == '__main__':
             critic_exp_mvg_avg = critic_exp_mvg_avg.detach()
 
             log_and_print(
-                '{} Epoch {}: Train [{}/{} ({:.1f}%)]\tR:{:.6f}\tuser_props: {:.6f}\tserver_props: {:.6f}'
-                .format(time.strftime('%H:%M:%S', time.localtime(time.time())),
-                        epoch, (batch_idx + 1) * len(user_seq), train_size,
-                        100. * (batch_idx + 1) / len(train_loader), torch.mean(reward),
-                        torch.mean(user_allocated_props),
-                        torch.mean(server_used_props)),
+                '{} Epoch {}: Train [{}/{} ({:.1f}%)]\tR:{:.6f}\tuser_props: {:.6f}\tserver_props: {:.6f}'.format(
+                    time.strftime('%H:%M:%S', time.localtime(time.time())),
+                    epoch,
+                    (batch_idx + 1) * len(user_seq),
+                    train_size,
+                    100. * (batch_idx + 1) / len(train_loader),
+                    torch.mean(reward),
+                    torch.mean(user_allocated_props),
+                    torch.mean(server_used_props)),
                 log_file_name)
 
         # Valid
@@ -129,12 +130,15 @@ if __name__ == '__main__':
                     = model(user_seq, server_seq, masks)
 
                 log_and_print(
-                    '{} Epoch {}: Valid [{}/{} ({:.1f}%)]\tR:{:.6f}\tuser_props: {:.6f}\tserver_props: {:.6f}'
-                    .format(time.strftime('%H:%M:%S', time.localtime(time.time())),
-                            epoch, (batch_idx + 1) * len(user_seq), valid_size,
-                            100. * (batch_idx + 1) / len(valid_loader), torch.mean(reward),
-                            torch.mean(user_allocated_props),
-                            torch.mean(server_used_props)),
+                    '{} Epoch {}: Valid [{}/{} ({:.1f}%)]\tR:{:.6f}\tuser_props: {:.6f}\tserver_props: {:.6f}'.format(
+                        time.strftime('%H:%M:%S', time.localtime(time.time())),
+                        epoch,
+                        (batch_idx + 1) * len(user_seq),
+                        valid_size,
+                        100. * (batch_idx + 1) / len(valid_loader),
+                        torch.mean(reward),
+                        torch.mean(user_allocated_props),
+                        torch.mean(server_used_props)),
                     log_file_name)
 
             # Test
@@ -157,10 +161,9 @@ if __name__ == '__main__':
             r = torch.mean(R_list)
             user_allo = torch.mean(user_allocated_props_list)
             server_use = torch.mean(server_used_props_list)
-            log_and_print('{} Epoch {}: Test \tR:{:.6f}\tuser_props: {:.6f}\tserver_props: {:.6f}'
-                          .format(time.strftime('%H:%M:%S', time.localtime(time.time())),
-                                  epoch, r, user_allo, server_use),
-                          log_file_name)
+            log_and_print('{} Epoch {}: Test \tR:{:.6f}\tuser_props: {:.6f}\tserver_props: {:.6f}'.format(
+                time.strftime('%H:%M:%S', time.localtime(time.time())), epoch, r, user_allo, server_use),
+                log_file_name)
             log_and_print('', log_file_name)
 
             test_reward_list.append(r)
@@ -196,10 +199,8 @@ if __name__ == '__main__':
 
             torch.cuda.empty_cache()
 
-            # 如果超过10个epoch奖励都没有再提升，就停止训练
-            if best_time >= 10:
-                log_and_print("训练结束，最好的reward:{}，用户分配率:{}，服务器租用率:{}"
-                              .format(best_r, best_user, best_server), log_file_name)
+            # 如果超过20个epoch奖励都没有再提升，就停止训练
+            if best_time >= 20:
                 log_and_print("效果如下：", log_file_name)
                 for i in range(len(test_reward_list)):
                     log_and_print("Epoch: {}\treward: {}\tuser_props: {}\tserver_props: {}"
@@ -211,6 +212,8 @@ if __name__ == '__main__':
                 state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': epoch}
                 torch.save(state, model_filename)
                 log_and_print("模型已存储到: {}".format(model_filename), log_file_name)
+                log_and_print("训练结束，最好的reward:{}，用户分配率:{}，服务器租用率:{}"
+                              .format(best_r, best_user, best_server), log_file_name)
                 exit()
 
             # if epoch % 20 == 19:
